1c287340409fbbb920ec85fb614a1d91f590cf33
Minghao <Minghao@minghaodemacbook-pro.local>
2016-11-02 14:48:30 +0800

    初始化项目！

---
+++ douyu/douyu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  douyu
+//
+//  Created by Minghao on 16/11/2.
+//  Copyright © 2016年 Minghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ douyu/douyu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  douyu
+//
+//  Created by Minghao on 16/11/2.
+//  Copyright © 2016年 Minghao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
