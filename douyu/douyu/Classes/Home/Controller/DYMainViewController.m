a643a94b5d1302ee0fe9ae0b3870cfa9254c95cb
Minghao <Minghao@minghaodemacbook-pro.local>
2016-11-02 15:50:14 +0800

    初始化项目，整理架构目录。

---
+++ douyu/douyu/Classes/Home/Controller/DYMainViewController.m
@@ -0,0 +1,32 @@
+//
+//  DYMainViewController.m
+//  douyu
+//
+//  Created by Minghao on 16/11/2.
+//  Copyright © 2016年 Minghao. All rights reserved.
+//
+
+#import "DYMainViewController.h"
+
+@interface DYMainViewController ()
+
+@end
+
+@implementation DYMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ douyu/douyu/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  douyu
+//
+//  Created by Minghao on 16/11/2.
+//  Copyright © 2016年 Minghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
